6be66fc0021171b6739874dfcf6d77dc9b78725f
milicamih <milicamih88@gmail.com>
2018-11-17 15:12:12 +0100

    puting auction objects on page

--- front-end-start/src/app/app.module.ts
+++ front-end-start/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-
+// COMPONENTS ----------------------------------------------------
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { NewComponent } from './auctions/new/new.component';
@@ -18,8 +18,8 @@ import { SearchComponent } from './auctions/search/search.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     NgbModule.forRoot(),
-
     AppRoutingModule
   ],
   bootstrap: [AppComponent]
--- front-end-start/src/app/auctions/search/search.component.ts
+++ front-end-start/src/app/auctions/search/search.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Auction } from '../model/auction';
+
 
 @Component({
   selector: 'kupi-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
+
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  auctions: Auction[] = [];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<Auction[]>('http://localhost:3000/api/auctions').subscribe(data => {
+      this.auctions = data;
+    }, error => {
+      console.log('Error while getting ...');
+    })
   }
 
 }
